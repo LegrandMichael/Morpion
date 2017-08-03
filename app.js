819069688269b834642f382967cbf1b60d47c5a3
LegrandMichael <michael.c.legrand@gmail.com>
2017-08-03 16:17:20 +0200

    Add call back reset

--- app.js
+++ app.js
@@ -1,8 +1,55 @@
 $(document).ready(function(){
+	// var player1 = prompt("Please, enter player one's name");
+	// var player2 = prompt("Please, enter player two's name");
+
+	// $(".P1").html(player1);
+	// $(".P2").html(player2);
+
+	var o = "<img src=O.svg alt=O>";
+	var x = "<img src=x.svg alt=X>";
+	var tour = 0;
+	
+	// var pos11 = $("#11").html();
+	// var pos12 = $("#12").html();
+	// var pos13 = $("#13").html();
+	// var pos21 = $("#21").html();
+	// var pos22 = $("#22").html();
+	// var pos23 = $("#23").html();
+	// var pos31 = $("#31").html();
+	// var pos32 = $("#32").html();
+	var pos33 = $("#33").html(o);
+
+	// $("#11").html(o);
+	// $("#12").html(o);	
+	// $("#13").html(o);
+	// $("#21").html(o);
+	// $("#22").html(o);
+	// $("#23").html(o);
+	// $("#31").html(o);
+	// $("#32").html(o);
+	// $("#33").html(o);
+
 
 	$(".case").click(function(){
+		// Cross and Circle placement on board
+		if (($(this).html() === "")) { // Begin if case is empty
+			if (tour%2 === 0) {
+				$(this).html(o);
+				tour++;
+			} else {
+				$(this).html(x);
+				tour++;
+			}
+		}
 
-		console.log("test");
-		
+
+
+
 	});
+// Reset function on click button 
+$("#reset").click(function(){
+	$(".case").html("");
+	tour = 0;
+
+});
 });
